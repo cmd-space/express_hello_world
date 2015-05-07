b58a5feed1a19f90a045cb1c4d7f366d0307b029
mec64 <masonecrane@gmail.com>
2015-05-06 21:04:15 -0600

    added static folder, as well as included it in server.js for static content

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app = express();
 app.get('/',function(request, response){
     response.send('<h1>Hello Express</h1>');
 })
+// tell express to use static folder for static content
+app.use(express.static(__dirname + '/static'));
 // tell express app to listen on port 8000
 app.listen(8000, function(){
     console.log('listening on port 8000');
