a2733acebab9b47795bffcb7af2ecb87db9378da
mec64 <masonecrane@gmail.com>
2015-05-06 21:40:00 -0600

    installed ejs, created view folder, created static array for demo purposes for /users view

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ var app = express();
 app.get('/',function(request, response){
     response.send('<h1>Hello Express</h1>');
 })
+app.set('view engine', 'ejs');
 // tell express to use static folder for static content
 app.use(express.static(__dirname + '/static'));
+app.set('views', __dirname + '/views');
+app.get('/users', function(request, response){
+    //hard coded user data
+    var users_array = [
+        {name: "Michael", email: "michael@codingdojo.com"},
+        {name: "Jay", email: "jay@codingdojo.com"},
+        {name: "Rory", email: "rory@codingdojo.com"},
+        {name: "Andrew", email: "andrew@codingdojo.com"}
+    ];
+    response.render('users', {users: users_array});
+})
 // tell express app to listen on port 8000
 app.listen(8000, function(){
     console.log('listening on port 8000');
