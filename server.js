c064a93886ba806cc1945f3b6d008ab2f79f4a15
mec64 <masonecrane@gmail.com>
2015-05-06 20:50:06 -0600

    installed express, and setup basic server

---
+++ server.js
@@ -0,0 +1,12 @@
+// Load the express module
+var express = require('express');
+// invoke var express and store the resulting application in var app
+var app = express();
+
+app.get('/',function(request, response){
+    response.send('<h1>Hello Express</h1>');
+})
+// tell express app to listen on port 8000
+app.listen(8000, function(){
+    console.log('listening on port 8000');
+})
